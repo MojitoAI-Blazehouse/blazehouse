b8bec66cc097a4e9990055103a75c632ce52d42f
Andy Lee <techandy42@gmail.com>
2025-10-05 10:39:33 -0700

    added mojito integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+;(globalThis as any).__MOJITO_ENABLED__ = '1'
+;(globalThis as any).__MOJITO_DEBUG__ = true
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,25 +10,28 @@ import Index from "./pages/Index";
 import BookNow from "./pages/BookNow";
 import BuyClasses from "./pages/BuyClasses";
 import NotFound from "./pages/NotFound";
+import { MojitoVariant } from '@mojito-ai/react';
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/book-now" element={<BookNow />} />
-          <Route path="/buy-classes" element={<BuyClasses />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <MojitoVariant id="entire-website">
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/book-now" element={<BookNow />} />
+            <Route path="/buy-classes" element={<BuyClasses />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </MojitoVariant>
 );
 
 export default App;
